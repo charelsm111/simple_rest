5b0f2143398043e57989aedcc209dea9e59812aa
Charel Samuel <Charel_SM111@mitrais.com>
2020-03-26 12:48:42 +0700

    initial commit

---
+++ src/main/java/org/anakinsky/simplerest/SimpleRest.java
@@ -0,0 +1,13 @@
+package org.anakinsky.simplerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRest.class, args);
+    }
+
+}
