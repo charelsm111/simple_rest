c7c62ecf4b618e1d1801923dfb5377631abeee13
Charel Samuel <Charel_SM111@mitrais.com>
2020-03-26 13:13:49 +0700

    change @Controller to @RestController

--- src/main/java/org/anakinsky/simplerest/controller/HomeController.java
+++ src/main/java/org/anakinsky/simplerest/controller/HomeController.java
@@ -1,12 +1,12 @@
 package org.anakinsky.simplerest.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home() {
 
         return "Home";
